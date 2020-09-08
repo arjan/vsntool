83f5fd301df76cd8fafe56eacc82e291ba7d5493
Arjan Scherpenisse <arjan@scherpenisse.net>
2020-09-08 22:28:57 +0200

    Explain the dokku plugin

--- lib/vsntool/plugin/dockerfile_dokku.ex
+++ lib/vsntool/plugin/dockerfile_dokku.ex
@@ -1,3 +1,12 @@
 defmodule Vsntool.Plugin.DockerfileDokku do
+  @moduledoc """
+  Patches the version in a root-level Dockerfile.dokku project.
+
+  I have the tendency to deploy simple applications to dokku
+  (http://dokku.viewdocs.io/dokku/); if so, I usually create a
+  dedicated Dockerfile for dokku which dokku then uses to run the
+  application from. Patching it with the version feels cleaner than
+  using a :latest tag in the Dockerfile.
+  """
   use Vsntool.RegexPatcher, file: "Dockerfile.dokku", regex: ~r/^FROM.*:(\d.*)$/
 end
